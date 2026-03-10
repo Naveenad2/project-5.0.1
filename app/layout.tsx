a9bc35e3c922bd7bfa840e99e296294ad1dbce45
Krishna <krishna@Naveens-MacBook-Air.local>
2026-03-10 14:21:44 +0530

    phase 10

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,11 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
